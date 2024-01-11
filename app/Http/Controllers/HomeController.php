7eb7faea2868847e4f1d0f68eb6c095e794ca7ce
moditha23 <102273283+moditha23@users.noreply.github.com>
2024-01-11 22:58:00 +0530

    Show Product to User

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,14 +4,16 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
-
 use Illuminate\Support\Facades\Auth;
+use App\Models\Product;
+
 class HomeController extends Controller
 {
 
     public function index()
     {
-        return view ('home.userpage');
+        $product=Product::all();
+        return view ('home.userpage',compact('product'));
 
 
     }

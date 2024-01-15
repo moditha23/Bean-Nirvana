fc40fb47299694e707951a42f3d7092bf1ccb7f8
moditha23 <102273283+moditha23@users.noreply.github.com>
2024-01-15 22:45:23 +0530

    part-15

--- resources/views/home/showcart.blade.php
+++ resources/views/home/showcart.blade.php
@@ -112,7 +112,7 @@
             <h1 style="font-size: 25px; padding-bottom: 15px;">Proceed to Order</h1>
 
             <a href="{{url('cash_order')}}" class="btn btn-danger">Cash on Delivery</a>
-            <a href="" class="btn btn-danger">Pay Using Card</a>
+            <a href="{{url('stripe',$totalprice)}}" class="btn btn-danger">Pay Using Card</a>
 
         </div>
 
--- routes/web.php
+++ routes/web.php
@@ -58,3 +58,7 @@ route::get('/show_cart',[HomeController::class,'show_cart']);
 route::get('/remove_cart/{id}',[HomeController::class,'remove_cart']);
 
 route::get('/cash_order',[HomeController::class,'cash_order']);
+
+route::get('/stripe/{totalprice}',[HomeController::class,'stripe']);
+
+Route::post('stripe/{totalprice}',[HomeController::class,'stripePost'])->name('stripe.post');
